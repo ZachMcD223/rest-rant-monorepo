4e41f0d0b167fa009494199ca0127559361d61c6
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-06-09 13:15:58 -0500

    added jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
-
+const jwt =require('json-web-token')
 const { User } = db
 
 router.post('/', async (req, res) => {
@@ -13,6 +13,9 @@ router.post('/', async (req, res) => {
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })           
+        res.json({ user: user, token: result.value })           
+    
         req.session.userId = user.userId
         console.log(req)
         res.json({ user })
